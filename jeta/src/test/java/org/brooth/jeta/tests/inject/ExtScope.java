994f9231d93a6982766d5d6a7853c7da31aa3495
broorh <broorh@gmail.com>
2016-03-26 15:15:31 +0400

    rel 1.0

--- jeta/src/test/java/org/brooth/jeta/tests/inject/ExtScope.java
+++ jeta/src/test/java/org/brooth/jeta/tests/inject/ExtScope.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2016 Oleg Khalidov
+ *  Copyright 2016 Oleg Khalidov
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  */
 
@@ -20,8 +20,7 @@ package org.brooth.jeta.tests.inject;
 import org.brooth.jeta.inject.Scope;
 
 /**
- * @author khalidov
- * @version $Id$
+ * @author Oleg Khalidov (brooth@gmail.com)
  */
 @Scope(ext = DefaultScope.class)
 public class ExtScope extends DefaultScope {
